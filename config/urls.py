4f1977d9dc6ee1b3ba8ab0a9871b7d06c5648865
denny <dennyrustandi1094@gmail.com>
2021-12-22 18:36:10 +0700

    Add auth url

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path
+
 
 urlpatterns = [
+    path('', include('dj_rest_auth.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls'))
 ]
