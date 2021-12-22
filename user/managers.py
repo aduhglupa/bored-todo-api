aa48964b9086495d2008dab06ca37f746772b671
denny <dennyrustandi1094@gmail.com>
2021-12-22 18:35:54 +0700

    Change user model schema

--- user/managers.py
+++ user/managers.py
@@ -1,3 +1,4 @@
+from allauth.account.models import EmailAddress
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext as _
 
@@ -17,6 +18,14 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_verified', True)
+        extra_fields.setdefault('is_anonymous', False)
 
-        return self.create_user(email, password, **extra_fields)
+        user = self.create_user(email, password, **extra_fields)
+
+        # Set super user to verified email
+        email_address = EmailAddress.objects.add_email(None, user, email)
+        email_address.verified = True
+        email_address.primary = True
+        email_address.save()
+
+        return user
--- user/models.py
+++ user/models.py
@@ -8,7 +8,7 @@ from user.managers import UserManager
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     is_active = models.BooleanField(default=False)
-    is_verified = models.BooleanField(default=False)
+    is_anonymous = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
