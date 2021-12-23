1ccc271b5a3c1303e5d2d55a883620eb9d579028
denny <dennyrustandi1094@gmail.com>
2021-12-23 21:42:11 +0700

    Tidy up installed apps

--- config/settings.py
+++ config/settings.py
@@ -44,16 +44,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
+    # Library App
     'django_extensions',
+
     'rest_framework',
     'rest_framework.authtoken',
+
     'dj_rest_auth',
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
     'dj_rest_auth.registration',
 
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    # App
     'common',
     'custom_auth',
     'user',
